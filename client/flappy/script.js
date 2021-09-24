734fd8fd5a136bc2e0bf245e2332316f674c11e2
jasmineokebugwu <jokebugw@umd.edu>
2021-09-24 14:41:25 -0400

    final completion of flappy bird

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded' , () => {
             }
             if (
                 obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 && 
-                birdBottom < obstacleBottom + 153 || 
+                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap -200) || 
                 birdBottom === 0) {
                 gameOver()
                 clearInterval(timerId)
